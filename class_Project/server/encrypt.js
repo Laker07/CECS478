45020f883c8040463dcf3d9a580ac2974809a980
Anthony Martinez <anthony@amartinez.cc>
2017-02-20 23:23:15 -0800

    forgeEnc.js added.  This has the encrypt function

--- class_Project/server/encrypt.js
+++ class_Project/server/encrypt.js
@@ -1,36 +1,79 @@
 
-var crypto = require('crypto')
+/* 
+
+rsa 2048 
+aes 64
+hmac 64
+iv 32
+
+*/
+
+
+
+var crypto = require('crypto') //publicc RSA  - for public encryp and pri decription
+var fs = require('fs')
+var constants = require('constants')
+//some file to place key
+
+//maybe use path and go files backwards!!!
+//this needs to change to a better whay of calling this file
+const rsak = fs.readFileSync('/public_key.pem', 'utf8')
+console.log(rsak);
+
 
 var text = "Test text, ha!"
-const key = crypto.randomBytes(32)
-const iv = crypto.randomBytes(16)
+const hmacKey = crypto.randomBytes(64).toString('utf8')
+const aesKey = crypto.randomBytes(64).toString('utf8') //aes and hmac 64
+
 	//for a list of aes options
 	//gist.github.com/reggi/4459803 
-const algo = 'aes-256-cbc'
+const algo = 'aes256'
 
 //not sure if to take these vars or  do 
 //encrypt(text
 
 //todo - return a json obj
-function encrypt(text, key, iv){	
-	//encrypt
-	let cipher = crypto.createCipheriv(algo, key, iv)
-	//cipher.update(data, input incoding, output encoding)
-	let encrypt = cipher.update(text, 'utf8', 'hex')
-	encrypt += cipher.final('hex')
+function encrypt(text){	
+	let iv = crypto.randomBytes(16)
 
-	//not sure if 'run on your ciphertext to compute integrity'
-	let hmac = crypto.createHmac('sha256', 'somekey?')
-	hmac.update('some data to hash')
+	//cipher object
+
+	//need some IV!!!!!!!!!!!!
 
 
 
+	//let cipher = crypto.createCipheriv(algo, aesKey, iv)
+	let cipher = crypto.createCipher(algo, aesKey)
+	let aesEnc = cipher.update(text)
+	aesEnc += cipher.final('hex')
 
-	console.log("encrypt: " + encrypt)
-	console.log("key: " + key)
-	console.log("iv: " + iv)
+	
+	let tag = crypto.createHmac('sha256', hmacKey)
+	tag.update(aesEnc).digest('hex')
 
+	//keys
+	let keys = aesKey + ":" + hmacKey
+	//r encryptStringWithRsaPublicKey = function(toEncrypt, relativeOrAbsolutePathToPublicKey) {
+   // var absolutePath = path.resolve(relativeOrAbsolutePathToPublicKey);
+    //var publicKey = fs.readFileSync(absolutePath, "utf8");
+    
+    var buffer = new Buffer(keys);
+    var rsaEnc = crypto.publicEncrypt({"key":rsak,padding:constants.RSA_PKCS1_PADDING},  
+    	buffer).toString("hex");
+
+
+	console.log("aes : " + aesEnc)
+	console.log("hmac: " + tag)
+	console.log("rsa : " + rsaEnc)
+
+	var obj ={
+		aesCipher : aesEnc,
+		hmacTag : tag,
+		rsaCipher : rsaEnc
+	}
+
+	return obj;
 }
 
-encrypt("hello", key, iv)
+encrypt("hello")
 
---
+++ class_Project/server/forgeEnc.js
@@ -0,0 +1,48 @@
+
+
+var forge = require('node-forge')
+var fs = require('fs')
+
+const rsak = fs.readFileSync(__dirname + '/../../../public_key.pem', 'utf8')
+
+let aesKey = forge.random.getBytesSync(32);
+let hmacKey = forge.random.getBytesSync(32);
+
+let encrypt = function encrypt(text){
+
+	//AES ENC	
+	let iv = forge.random.getBytesSync(16)
+	let cipher = forge.cipher.createCipher('AES-CBC', aesKey)
+	cipher.start({iv:iv})
+	cipher.update(forge.util.createBuffer(iv +":"+ text))
+	cipher.finish()
+	aesEncrypted = cipher.output;
+	//console.log(aesEncrypted.toHex())
+	
+	//hmac
+	let hmac = forge.hmac.create()
+	hmac.start('sha256', hmacKey)
+	hmac.update(aesEncrypted.toHex())
+	let tag = hmac.digest();
+	//console.log(tag.toHex())
+
+	//rsa
+	var publicKey = forge.pki.publicKeyFromPem(rsak)
+	let combined = aesKey+":"+hmacKey
+	var rsaCipher = publicKey.encrypt(combined)
+	//console.log(rsaCipher)
+
+
+	var obj ={
+		aesCipher : aesEncrypted.toHex(),
+		hmacTag : tag.toHex(),
+		rsaCipher : rsaCipher
+	}
+
+	return obj; 
+}
+
+console.log(encrypt("hello world"))
+
+
+module.export = encrypt
