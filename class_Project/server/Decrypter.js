00abe4fa54fc91f0bf181611c3729e8787238ab0
Michael Munoz <munoz1michael@gmail.com>
2017-02-28 23:57:49 -0800

    Comments on Decypter
    Added comments and cleaned up commented out code

--- class_Project/server/Decrypter.js
+++ class_Project/server/Decrypter.js
@@ -10,24 +10,25 @@ module.exports = {
     decryptMessage: function(JSONInput) {
         
        
-        //console.log(fileSystem.existsSync("./../../private_key.pem"));        
+              
         let privateKey = forge.pki.privateKeyFromPem(fileSystem.readFileSync("./../../../private_key.pem"));
         
-        //change the key for rsaobject
+        //Dectyptes the rsa, the first 32 bytes are the aes key and the remaining 32 are the hmac key
         let decryptedRSA = privateKey.decrypt(JSONInput['rsaCipher'], 'RSA-OAEP');
         let aesKey = decryptedRSA.substring(0, 32);
         let hmacKey = decryptedRSA.substring(32);
         
+        //create our own hmac
         let hmac = forge.hmac.create();
         hmac.start('sha256', hmacKey);
         hmac.update(JSONInput['aesCipher']);
 
+        //if they do not match, then an error has occured
         if(JSONInput['hmacTag'] != hmac.digest().toHex())
             throw "ERROR WITH THE TAGS";
        
-
-
-        
+        //create a aes decipher, iv is the first 16 bytes of the aes cipher,
+        //the remaning bytes are the ciphertext
         let decipher = forge.cipher.createDecipher('AES-CBC', aesKey);
         let iv = JSONInput['aesCipher'].substring(0, 16);
         decipher.start({iv: iv});
