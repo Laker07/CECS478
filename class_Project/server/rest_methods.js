036b308af8a9c991f5dbc93c1330377b0959c13d
Michael Munoz <munoz1michael@gmail.com>
2017-03-16 21:18:10 -0700

    JWT + JSON inputs
    Added JWT support for each method
    
    Register - post
    Params: JSON object that contains the username and password
    Returns: JSON object that stats error or success, on success returns a
    JWT token
    
    Sign In - get
    Params: JSON object that contains the username and  password
    Return: JSON object that states error or success, on success returns
    JWT token
    
    send message  - post
    Params: JWT token, JSON token containing message, receiver name and
    timestamp
    Returns: JSON object stating success or error
    
    get message - get
    Params: JWT token
    Returns: JSON object that stats success or error, on success returns
    all the messages

--- class_Project/server/rest_methods.js
+++ class_Project/server/rest_methods.js
@@ -1,58 +1,107 @@
-//new comment
-
-//simple example
 var express = require("express");
 var mysql = require('mysql');
 var connection = mysql.createConnection({
    host     : 'localhost',
    user     : 'root',
    password : '',
-   database : 'ServerDB'
+   database : 'ServerDB2'
  });
- 
+var fileSystem = require('fs');
+var jwt = require('jsonwebtoken');
+
 var app = express();
 
-
 app.get("/", function(req,res){
-
 
  res.send("welcome to cecs.me");
 });
 
 
-//used for adding a new user to db
-app.put("/newuser",function(req,res) {
+app.post("/register", function(req, res) {
   
-  //check that the values were provided
-  if((typeof req.query["username"] === "undefined") || (typeof req.query["password"] === "undefined"))
-    return res.send("Error with inputs");
+  //check if the secret file even exits...
+  if (!fileSystem.existsSync('secret.txt'))
+    return res.send({'response': 'Error', 'error': 'secret does not exist'});
+  //check if the parameter data was supplied
+  if(typeof req.query["data"] === "undefined")
+    return res.send({'response': 'Error', 'error': 'Parameter is undefined'});
+  var userInput = JSON.parse(req.query["data"]);
   
-  var addNewUserQuery = "INSERT INTO users (username, password) VALUES (\"" + req.query["username"] + "\", \"" + req.query["password"] +"\");";
-  //insert new user to db
+  var addNewUserQuery = "INSERT INTO users (username, password) VALUES (\"" + userInput['username'] + "\", \"" + userInput['password'] +"\");";
+  
   connection.query(addNewUserQuery, function(err, results, fields) {
     if (err)
-      //error occures, exit function
-      return res.send("Error executing query " + err);
-      else
-        return res.send(req.query["username"] + " added to db");
+      //error occured, exit function
+      return res.send({'response': 'Error', 'error': err});
+    else {
+      //create jwt token
+      var secret = fileSystem.readFileSync('secret.txt', 'utf8');
+      //token is encoded with hmac SHA256 by default
+      var jwtToken = jwt.sign({'username': userInput['username']}, secret);
+      return res.send({'response': 'Success', 'message': "JWT token generated and user register", 'jwtToken':jwtToken});   
+    }
   });
-
 });
+
+//used to verify if the user can sign into app
+app.get("/signIn", function(req, res) {
+
+  //check if the secret file even exits...
+  if (!fileSystem.existsSync('secret.txt'))
+    return res.send({'response': 'Error', 'error': 'secret does not exist'});
+  //check if the parameter data was supplied
+  if(typeof req.query["data"] === "undefined")
+    return res.send({'response': 'Error', 'error': 'Parameter is undefined'});
+  
+  var userInput = JSON.parse(req.query["data"]);
+  
+  //check here if password and username are the same in database
+  var signInQuery="SELECT EXISTS (SELECT 1 FROM users WHERE username=\"" + userInput["username"] + "\" AND password=\"" + userInput["password"] + "\");";
+  connection.query(signInQuery, function(err, results, fields) {
+    if (err)
+      //error occures, exit function
+      return res.send({'response': 'Error', 'error': err});
+    else
+      if(results[0][signInQuery.substring(7, signInQuery.length - 1)]) {
+        //create jwt token
+        var secret = fileSystem.readFileSync('secret.txt', 'utf8');
+        //token is encoded with hmac SHA256 by default
+        var jwtToken = jwt.sign({'username': userInput['username']}, secret);
+        return res.send({'response': 'Success', 'message': "JWT token generated and user signed in", 'jwtToken':jwtToken});   
+      }
+      else 
+        return res.send({'response': 'Error', 'error': 'Incorrect username or password'});
+  });
+});
+
 
 //used for sending a message
 app.post("/sendMessage",function(req,res) {
 
-  if((typeof req.query["username"] === "undefined") || (typeof req.query["receiverName"] === "undefined") || (typeof req.query["message"] === "undefined"))
-    return res.send("Error with inputs");
-  var sendMessageQuery = "INSERT INTO messages(username, receiverName, message) VALUES (\"" + req.query["username"] + "\", \"" + req.query["receiverName"] + "\", \"" + req.query["message"] + "\");"
+  //check the parameters 
+  if((typeof req.query["token"] === "undefined") || (typeof req.query["data"] === "undefined"))
+    return res.send({'response': 'Error', 'error': "wrong inputs"});
+  //load secret
+  var secret = fileSystem.readFileSync('secret.txt', 'utf8');
+
+  var decoded ="";
+  try {//verify the token
+    decoded = jwt.verify(req.query['token'], secret);
+  } catch (Error) {//does not pass the verification, exit method
+    return res.send({'response': 'Error', 'error': "error"});
+  }
+  //data is in a json object so pare its
+  var userInput = JSON.parse(req.query["data"]);
+
+  var sendMessageQuery = "INSERT INTO messages(username, receiverName, message, timestamp) VALUES (\"" + decoded['username'] + "\", \"" + userInput["receiverName"] + "\", \"" + userInput["message"] + "\", \"" + userInput["timeStamp"] + "\");";
 
   //put message into databse
   connection.query(sendMessageQuery, function(err, results, fields) {
     if (err)
       //error occures, exit function
-      return res.send("Error executing query " + err);
+      return res.send({'response': 'Error', 'error': err});
     else
-      return res.send("Message sent to: " + req.query["receiverName"])
+      return res.send({'response': 'Success', 'Message': "Message sent to " + userInput["receiverName"]});
   });
 });
 
@@ -60,15 +109,25 @@ app.post("/sendMessage",function(req,res) {
 app.get("/getMessages", function(req, res) {
   
   //if no id is provided end the request  
-  if(typeof req.query["username"] === "undefined")
-    return res.send("No user id provided");
+  if(typeof req.query["token"] === "undefined")
+    return res.send({'response': 'Error', 'error': "wrong inputs"});
+  
+  //open the secret
+  var secret = fileSystem.readFileSync('secret.txt', 'utf8');
+
+  var decoded ="";
+  try {//verify the jwt token
+    decoded = jwt.verify(req.query['token'], secret);
+  } catch (Error) {//does not pass the verification, exit method
+    return res.send({'response': 'Error', 'error': "error"});
+  }
   
   //execute the query in the db
-  var getMessageQuery ="SELECT * FROM messages WHERE receiverName=\""+req.query["username"]+"\";";
+  var getMessageQuery ="SELECT * FROM messages WHERE receiverName=\""+decoded["username"]+"\";";
   connection.query(getMessageQuery, function(err, results, fields) {
     if (err)
       //error occures, exit function
-      return res.send("Error executing query " + err);
+      return res.send({'response': 'Error', 'error': err});
     else {
       var jsonArray = new Array();
       //user can have multiple messages waiting for them so create an array to store them all
@@ -76,44 +135,24 @@ app.get("/getMessages", function(req, res) {
         //put each message with the users 
         jsonArray[i] = {
           message:results[i]["message"],
-          sender:results[i]["username"]
+          sender:results[i]["username"],
+          timestamp:results[i]["timestamp"]
         }  
       }
-
-      var deleteMyMessagesQuery = "DELETE FROM messages WHERE receiverName=\""+req.query["username"]+"\";";
+      
+      var deleteMyMessagesQuery = "DELETE FROM messages WHERE receiverName=\""+ decoded["username"]+"\";";
       //delete the messages from the database since they are being sent to user
       connection.query(deleteMyMessagesQuery, function(err, results, fields) {
         if(err)
-          return res.send("Error executing query " + err);
+          return res.send({'response': 'Error', 'error': err});
         else
           //return json array of messages
-          return res.json(jsonArray);
+          return res.json({'response': 'Success', 'messagecount': i, "messages": jsonArray});
       });
     }
   });  
 });
 
-//used to verify if the user can sign into app
-app.get("/signIn", function(req, res) {
-
-  //check that the values were provided
-  if((typeof req.query["username"] === "undefined") || (typeof req.query["password"] === "undefined"))
-    return res.send("Error with inputs");
-  //check here if password and username are the same in database, if they are then return success else nope
-  var signInQuery="SELECT EXISTS (SELECT 1 FROM users WHERE username=\"" + req.query["username"] + "\" AND password=\"" + req.query["password"] + "\");";
-  connection.query(signInQuery, function(err, results, fields) {
-    if (err)
-      //error occures, exit function
-      return res.send("Error executing query " + err);
-    else
-      if(results[0][signInQuery.substring(7, signInQuery.length - 1)])
-        return res.send("Auth");
-      else 
-        return res.send("Fail");
-  });
-
-
-});
 
 
 app.listen(8080, 'localhost',function(){
@@ -122,23 +161,26 @@ console.log('listening on 8080');
 });
 
 /*
-put
-localhost:8080/newuser?username=jynx&password=123qwerty
-post
-localhost:8080/sendMessage?username=jynx&receiverName=jane1&message=hey
-get
-localhost:8080/getMessages?username=jane1
-get
-localhost:8080/signIn?username=jane1&password=123qwerty
+NOTES:
+
+register - post
+localhost:8080/register?data={"username":"34545", "password":"unknown"}
+signIn - get
+localhost:8080/signIn?data={"username":"34545", "password":"unknown"}
+localhost:8080/signIn?data={"username":"chrome24", "password":"mike"}
+sendMessage - post
+localhost:8080/sendMessage?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IjM0NTQ1IiwiaWF0IjoxNDg5NzE5MzMxfQ.DwnKx74nKWHJja4ixWDpw0ia2wv2LRktFqfl-1NoYHY&data={"receiverName":"chrome24", "message":"hello, test message", "timeStamp":"2017-03-16 02:23:53"}
+getMessage - get
+localhost:8080/getMessages?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImNocm9tZTI0IiwiaWF0IjoxNDg5NzIzMDIxfQ.fdMmxgmBvNmIQ02cb19R07oziRhtXRNSKiifubxIWgE
 
 
-table users
-userName VARCHAR(30) pk
-password VARCHAR(255)
+adding messages to db
+INSERT INTO messages(username, receiverName, message, timestamp) 
+VALUES ("34545", "34545", "somemessage", '2017-03-16 02:23:53');
 
-table messages
-userName VARCHAR(30)fk
-receiverName VARCHAR(30)
-message id int auto increment pk
-message varchar(500)
+
+create random characters for secret
+openssl rand -out secret.txt -base64 100
+
+
 */
